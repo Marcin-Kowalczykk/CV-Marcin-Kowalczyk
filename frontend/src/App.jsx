18d7434c27858c74882b9d1a1f381f3f003246f8
Marcin <marcin.kowalczyk@dacsoftware.pl>
2025-06-03 10:01:48 +0200

    Backend + gh pages for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const isEnvLocal = false;
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const apiUrlLocal = import.meta.env.VITE_API_URL_LOCAL;
+
+  console.log('Environment variables:', {
+    VITE_API_URL: import.meta.env.VITE_API_URL,
+    VITE_API_URL_LOCAL: import.meta.env.VITE_API_URL_LOCAL
+  });
+
+  const fetchMessage = async () => {
+    setIsLoading(true);
+    const response = await fetch(`${isEnvLocal ? apiUrlLocal : apiUrl}/api/message`);
+    const data = await response.json();
+
+    setMessage(data.message);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Frontend Vite + React + Express</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>Backend message: {message}</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
